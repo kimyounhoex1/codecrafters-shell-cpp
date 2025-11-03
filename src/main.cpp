36bbcafce4854578fb7d5e487b75e622a5fa388a
kimyounhoex1 <dordyd11@gmail.com>
2025-11-03 22:59:14 +0900

    Implement a REPL

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,13 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  std::string input;
-  std::cin >> input;
-  
-  std::string printErr = "command not found";
-  
-  std::cout << input << ": " << printErr << std::endl;
+  while(true){
+    std::cout << "$ ";
+    std::string input;
+    std::cin >> input;
+    
+    std::string printErr = "command not found";
+    
+    std::cout << input << ": " << printErr << std::endl;
+  }
 }
