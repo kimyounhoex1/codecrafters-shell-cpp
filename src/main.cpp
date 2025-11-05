b0a0974a012f3d007e2d0a2e8e5dc219e0f60c0b
kimyounhoex1 <dordyd11@gmail.com>
2025-11-05 22:17:39 +0900

    4

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
+
+std::vector<std::string> split(std::string& input, char delimiter = ' ') {
+  std::vector<std::string> tokens;
+  std::istringstream iss(input);
+  std::string token;
+  while(std::getline(iss, token, delimiter)) {
+    if(!token.empty()) {
+      tokens.push_back(token);
+    }
+  }
+
+  return tokens;
+}
+
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::string printErr = "command not found";
+  uint64_t idx;
   // TODO: Uncomment the code below to pass the first stage
   while(true){
     std::cout << "$ ";
     std::string input;
-    std::getline(std::cin, input);
-
-    if(input == "exit 0") {
-      return 0; //success
-    }
-    else if (input == "exit 1") {
-      return 1; // err
-    }
-    std::string printErr = "command not found";
+    idx = 0;
+    if(!std::getline(std::cin, input))
+      break;
     
-    std::cout << input << ": " << printErr << std::endl;
+    if(input.empty())
+      continue;
+    
+    auto args = split(input);
+
+    std::string builtin = args[idx++];
+
+    if(builtin == "exit" && args.size() > 1) {
+      std::cout << printErr;
+      // if(args[1] == "1") {
+      //   return 1;
+      // }
+      // else if(args[1] == "0") {
+      //   return 0;
+      // } 
+    }
+
+    if(builtin == "echo") {
+      std::vector<std::string> printEcho;
+      for(; idx < args.size(); idx++) {
+        std::cout << args[idx] << " ";
+      }
+    }
+    
+    std::cout << std::endl;
   }
 }
